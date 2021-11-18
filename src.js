84f51ea6b5f936741456047c8bf6705672950788
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-11-18 16:54:22 -0500

    Update src.js

--- src.js
+++ src.js
@@ -5,33 +5,33 @@ const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, v
     let porcentajeHC = limiteHC ? valorHC * 100 / limiteHC : 0;
     let porcentajeO2 = limiteO2 ? valorO2 * 100 / limiteO2 : 0;
 
-return {porcentajeCO,porcentajeCO2,porcentajeHC,porcentajeO2};
+    return {porcentajeCO,porcentajeCO2,porcentajeHC,porcentajeO2};
 }
 
 const registrarCO = (valorCO) => {
-    let msg = (valorCO >= 0 && valorCO <= 10) ? 'Parametro CO en rango estandar' : (valorCO >= 11 && valorCO <= 15) ? 'Parametro CO fuera de rango' : 'fuera_de_rango';
-    return msg;
+    let etiqueta = (valorCO >= 0 && valorCO <= 10) ? 'Parametro CO en rango estandar' : (valorCO >= 11 && valorCO <= 15) ? 'Parametro CO fuera de rango' : 'fuera_de_rango';
+    return etiqueta;
 }
 module.exports.registrarCO = registrarCO;
 module.exports.calcularPorcentajes = calcularPorcentajes;
 
 const registrarCO2 = (valorCO2) => {
-    let msg = (valorCO2 >= 0 && valorCO2 <= 20) ? 'Parametro CO2 en rango estandar' : (valorCO2 >= 21 && valorCO2 <= 30) ? 'Parametro CO2 fuera de rango' : 'fuera_de_rango';
-    return msg;
+    let etiqueta = (valorCO2 >= 0 && valorCO2 <= 20) ? 'Parametro CO2 en rango estandar' : (valorCO2 >= 21 && valorCO2 <= 30) ? 'Parametro CO2 fuera de rango' : 'fuera_de_rango';
+    return etiqueta;
 }
 module.exports.registrarCO2 = registrarCO2;
 module.exports.calcularPorcentajes = calcularPorcentajes;
 
 const registrarHC = (valorHC) => {
-    let msg = (valorHC >= 0 && valorHC <= 10000) ? 'Parametro HC en rango estandar' : (valorHC >= 10001 && valorHC <= 11000) ? 'Parametro HC fuera de rango' : 'fuera_de_rango';
-    return msg;
+    let etiqueta = (valorHC >= 0 && valorHC <= 10000) ? 'Parametro HC en rango estandar' : (valorHC >= 10001 && valorHC <= 11000) ? 'Parametro HC fuera de rango' : 'fuera_de_rango';
+    return etiqueta;
 }
 module.exports.registrarHC = registrarHC;
 module.exports.calcularPorcentajes = calcularPorcentajes;
 
 const registrarO2 = (valorO2) => {
-    let msg = (valorO2 >= 0 && valorO2 <= 22) ? 'Parametro O2 en rango estandar' : (valorO2 >= 23 && valorO2 <= 30) ? 'Parametro O2 fuera de rango' : 'fuera_de_rango';
-    return msg;
+    let etiqueta = (valorO2 >= 0 && valorO2 <= 22) ? 'Parametro O2 en rango estandar' : (valorO2 >= 23 && valorO2 <= 30) ? 'Parametro O2 fuera de rango' : 'fuera_de_rango';
+    return etiqueta;
 }
 module.exports.registrarO2 = registrarO2;
 module.exports.calcularPorcentajes = calcularPorcentajes;
