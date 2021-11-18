2a003673b75303dcccc87e2210d3bd5b3a526676
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-11-18 18:14:02 -0500

    Update src.js
    Se quita r a la función, queda:
    calcularPocentaje como aparece en  el backlog

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-const calcularPorcentajes = (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2) => {
+const calcularPocentajes = (limiteCO, limiteCO2, limiteHC, limiteO2, valorCO, valorCO2, valorHC, valorO2) => {
 
     let porcentajeCO = limiteCO ? valorCO * 100 / limiteCO : 0;
     let porcentajeCO2= limiteCO2 ? valorCO2 * 100 / limiteCO2 : 0;
@@ -13,28 +13,28 @@ const registrarCO = (valorCO) => {
     return etiqueta;
 }
 module.exports.registrarCO = registrarCO;
-module.exports.calcularPorcentajes = calcularPorcentajes;
+module.exports.calcularPocentajes = calcularPocentajes;
 
 const registrarCO2 = (valorCO2) => {
     let etiqueta = (valorCO2 >= 0 && valorCO2 <= 20) ? 'Parametro CO2 en rango estandar' : (valorCO2 >= 21 && valorCO2 <= 30) ? 'Parametro CO2 fuera de rango' : 'fuera_de_rango';
     return etiqueta;
 }
 module.exports.registrarCO2 = registrarCO2;
-module.exports.calcularPorcentajes = calcularPorcentajes;
+module.exports.calcularPocentajes = calcularPocentajes;
 
 const registrarHC = (valorHC) => {
     let etiqueta = (valorHC >= 0 && valorHC <= 10000) ? 'Parametro HC en rango estandar' : (valorHC >= 10001 && valorHC <= 11000) ? 'Parametro HC fuera de rango' : 'fuera_de_rango';
     return etiqueta;
 }
 module.exports.registrarHC = registrarHC;
-module.exports.calcularPorcentajes = calcularPorcentajes;
+module.exports.calcularPocentajes = calcularPocentajes;
 
 const registrarO2 = (valorO2) => {
     let etiqueta = (valorO2 >= 0 && valorO2 <= 22) ? 'Parametro O2 en rango estandar' : (valorO2 >= 23 && valorO2 <= 30) ? 'Parametro O2 fuera de rango' : 'fuera_de_rango';
     return etiqueta;
 }
 module.exports.registrarO2 = registrarO2;
-module.exports.calcularPorcentajes = calcularPorcentajes;
+module.exports.calcularPocentajes = calcularPocentajes;
 
 
 
